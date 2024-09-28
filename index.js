a1a1a2daae2b47629c84489150fa0af31ca3e8c8
Arni <arnifa@gmail.com>
2024-09-28 10:29:00 +0000

    Simplify things

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
-const Gpio = require('onoff').Gpio; // For GPIO control
-const { playScarySonos, playHalloweenSonos } = require("./sonos");
+const Gpio = require('pigpio').Gpio;
+const { playScarySonos } = require("./sonos");
 
-const MOTION_SENSOR_PIN = 17; // GPIO pin for motion sensor
-const RESET_TIME_IN_SECONDS = 120;
-const RESET_TIME = RESET_TIME_IN_SECONDS * 1000;
-let timer;
+const MOTION_SENSOR_PIN = 4; // Updated to GPIO 4
 
 // Initialize the motion sensor pin
-const motionSensor = new Gpio(MOTION_SENSOR_PIN, 'in', 'both');
+const motionSensor = new Gpio(MOTION_SENSOR_PIN, {
+  mode: Gpio.INPUT,
+  alert: true, // Use alert to detect changes in pin state
+});
 
-// Function to handle motion detection
-const onMotionDetected = () => {
-  console.log("Motion detected!");
-  playScarySonos();
-
-  clearTimeout(timer); // Reset timer
-  timer = setTimeout(() => {
-    playHalloweenSonos();
-  }, 10000);
-
-  // Reset logic after the defined reset time
-  setTimeout(() => {
-    console.log("Resetting...");
-    // Optionally reset any additional logic here
-  }, RESET_TIME);
-};
-
-// Listen for motion detection events
-motionSensor.watch((err, value) => {
-  if (err) {
-    console.error("Error detecting motion:", err);
-    return;
-  }
-  // Trigger on motion detected (value === 1 when motion is detected)
-  if (value === 1) {
-    onMotionDetected();
+// Handle motion detection
+motionSensor.on('alert', (level) => {
+  if (level === 1) {
+    console.log("Motion detected!");
+    playScarySonos();
   }
 });
 
 process.on('SIGINT', () => {
-  motionSensor.unexport(); // Unexport GPIO to clean up
+  console.log("Exiting...");
+  process.exit();
 });
--- sonos.js
+++ sonos.js
@@ -1,105 +1,62 @@
+
 const { Sonos } = require("sonos");
-const fs = require("fs");
 
 const volume = 100;
-const music_volume = 60;
 
 const sonos = new Sonos("10.0.1.140");
 
-const soundsFolder = "./sounds/";
+// Your base GitHub URL
+const soundBaseUrl = "https://github.com/arnif/hall--v-n/raw/main/sounds/";
 
-const soundPath = "https://github.com/arnif/hall--v-n/raw/main/sounds/";
-
-const fileNames = fs.readdirSync(soundsFolder).map((file) => {
-  return file;
-});
+// Sound files hosted on GitHub
+const fileNames = [
+  "Creepy_Drone_1.mp3",
+  "Creepy_Drone_2.mp3",
+  "Evil_Girl.mp3",
+  "Ghostly_Breath.mp3",
+  "Ghostly_Whisper.mp3",
+  "Igor.mp3",
+  "Jigsaws puppet laugh.mp3",
+  "Knock_Knock.mp3",
+  "Ring-around-the-rosie-creepy-voice.mp3",
+  "The_Scarecrow.mp3",
+  "Werewolf.mp3",
+  "Zombie Whuahrrrr Group.mp3",
+  "Zombie Whuahrrrr.mp3",
+  "alien breathing.mp3",
+  "dracula.mp3",
+  "game.mp3",
+  "hahahahahaha.mp3",
+  "witch female laugh.mp3",
+  "witch male laugh.mp3"
+];
 
 function clearSonos() {
-  // sonos.stop();
-  sonos.flush();
+  sonos.flush().then(() => {
+    console.log("Sonos queue cleared");
+  }).catch((error) => {
+    console.log("Error clearing Sonos queue:", error);
+  });
 }
 
 function playScarySonos() {
-  //   const item = fileNames[Math.floor(Math.random() * fileNames.length)];
-  //   console.log("Playing", item);
-  //   sonos
-  //     .play(soundPath + item)
-  //     .then((results) => {
-  //       console.log(JSON.stringify(results, null, 2));
-  //     })
-  //     .catch((error) => {
-  //       console.log(JSON.stringify(error, null, 2));
-  //     });
   sonos.setVolume(volume);
-  sonos
-    .getPlaylist("0", { start: 0, total: 25 })
-    .then((results) => {
-      const items = results.items;
 
-      const item = items[Math.floor(Math.random() * items.length)];
-      console.log("playing", item);
-      sonos
-        .play(item.uri)
-        .then((results) => {
-          console.log(JSON.stringify(results, null, 2));
-        })
-        .catch((error) => {
-          console.log(JSON.stringify(error, null, 2));
-        });
+  // Play a random sound from GitHub
+  const item = fileNames[Math.floor(Math.random() * fileNames.length)];
+  const soundUrl = soundBaseUrl + encodeURIComponent(item); // URL-encode in case of spaces
+  console.log("Playing remote sound from GitHub:", soundUrl);
+
+  sonos
+    .play(soundUrl)
+    .then((results) => {
+      console.log("Remote sound played:", JSON.stringify(results, null, 2));
     })
-    .catch((err) => {
-      console.log("Error occurred %j", err);
+    .catch((error) => {
+      console.log("Error playing remote sound:", JSON.stringify(error, null, 2));
     });
 
-  sonos.getVolume().then((volume) => console.log(`current volume = ${volume}`));
-  // sonos.setVolume(100);
+  sonos.getVolume().then((volume) => console.log(`Current volume = ${volume}`));
 }
 
-function playHalloweenSonos() {
-  // clearSonos();
-  //   const item = fileNames[Math.floor(Math.random() * fileNames.length)];
-  //   console.log("Playing", item);
-  //   sonos
-  //     .play(soundPath + item)
-  //     .then((results) => {
-  //       console.log(JSON.stringify(results, null, 2));
-  //     })
-  //     .catch((error) => {
-  //       console.log(JSON.stringify(error, null, 2));
-  //     });
-  sonos.setVolume(music_volume);
-  sonos
-    .getPlaylist("1", { start: 0, total: 25 })
-    .then((results) => {
-      const items = results.items;
-
-      // randomize items
-      const randomItems = items.sort(() => Math.random() - 0.5);
-
-      // const item = items[Math.floor(Math.random() * items.length)];
-      // console.log("playing", item);
-      // set items in queue
-      randomItems.forEach((item) => {
-        console.log("adding", item.title);
-        sonos.queue(item.uri);
-      });
-
-      sonos
-        .play()
-        .then((results) => {
-          console.log("done playing...play next..");
-          console.log(JSON.stringify(results, null, 2));
-        })
-        .catch((error) => {
-          console.log(JSON.stringify(error, null, 2));
-        });
-    })
-    .catch((err) => {
-      console.log("Error occurred %j", err);
-    });
-
-  sonos.getVolume().then((volume) => console.log(`current volume = ${volume}`));
-  // sonos.setVolume(100);
-}
-
-module.exports = { playScarySonos, playHalloweenSonos, clearSonos };
+module.exports = { playScarySonos, clearSonos };
