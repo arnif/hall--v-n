334c3bd2a31ade864e1f7dda307ade0598d0f02f
Arni <arnifa@gmail.com>
2024-09-28 14:10:42 +0000

    Fixing wled

--- wled.js
+++ wled.js
@@ -1,37 +1,39 @@
-const { WledClient } = require('wled-client');
+const { WLEDClient } = require("wled-client");
 
+// Sleep utility function
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// List of WLED device IP addresses
 // List of your WLED devices' IP addresses
 const wledInstances = [
-  '10.0.1.50', // Cube
-  '10.0.1.8' // arcade machine
+  "10.0.1.50", // Cube
+  "10.0.1.8", // arcade machine
 ];
 
-// Initialize WLED clients for each instance
-const wledClients = wledInstances.map(ip => new WledClient(ip));
-
-// Function to blink WLEDs red for 10 seconds and then return to white
+// Function to blink WLEDs red for 10 seconds and return to white
 async function blinkWLEDs() {
   try {
-    console.log('Turning WLEDs red...');
+    // Initialize all WLED clients and set them to red
+    for (const ip of wledInstances) {
+      const wled = new WLEDClient(ip);
+      await wled.init();
+      console.log(`Device ${ip} ready: version ${wled.info.version}`);
 
-    // Set all WLED instances to red (RGB: 255, 0, 0)
-    await Promise.all(
-      wledClients.map(client => client.setColor({ r: 255, g: 0, b: 0 }))
-    );
+      console.log(`Setting ${ip} to red...`);
+      await wled.setColor({ r: 255, g: 0, b: 0 }); // Set to red
 
-    // Wait for 10 seconds
-    await new Promise(resolve => setTimeout(resolve, 10000));
+      // Wait for 10 seconds
+      await sleep(10000);
 
-    console.log('Turning WLEDs back to white...');
+      console.log(`Setting ${ip} back to white...`);
+      await wled.setColor({ r: 255, g: 255, b: 255 }); // Set back to white
 
-    // Return all WLED instances to white (RGB: 255, 255, 255)
-    await Promise.all(
-      wledClients.map(client => client.setColor({ r: 255, g: 255, b: 255 }))
-    );
-
-    console.log('WLEDs returned to white.');
+      wled.disconnect(); // Disconnect from the WLED device
+    }
   } catch (error) {
-    console.error('Error controlling WLEDs:', error);
+    console.error("Error controlling WLEDs:", error);
   }
 }
 
