9156deda0a74057a49a4f9eeeb05d1aeef21254e
Arni <arnifa@gmail.com>
2024-10-22 17:50:17 +0000

    Prettier

--- animatronics.js
+++ animatronics.js
@@ -1,40 +1,36 @@
-
-
 const theThingHand = {
-    name: 'The Thing Hand',
-    startApi: '10.0.1.238/wave',
-    endApi: '10.0.1.238/stop'
-}
+  name: "The Thing Hand",
+  startApi: "10.0.1.238/wave",
+  endApi: "10.0.1.238/stop",
+};
 
 const zombieHand = {
-    name: 'Zombie Hand',
-    startApi: '10.0.1.167/raise',
-    endApi: '10.0.1.167/rest'
-}
+  name: "Zombie Hand",
+  startApi: "10.0.1.167/raise",
+  endApi: "10.0.1.167/rest",
+};
 
 const animatronics = [theThingHand, zombieHand];
 
-
 function startAllAnimatronics() {
-    animatronics.forEach(animatronic => {
-        fetch(`http://${animatronic.startApi}`)
-            .then(res => res.text())
-            .then(data => console.log(data))
-            .catch(err => console.log(err));
-    });
+  animatronics.forEach((animatronic) => {
+    fetch(`http://${animatronic.startApi}`)
+      .then((res) => res.text())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  });
 }
 
-
 function stopAllAnalimatronics() {
-    animatronics.forEach(animatronic => {
-        fetch(`http://${animatronic.endApi}`)
-            .then(res => res.text())
-            .then(data => console.log(data))
-            .catch(err => console.log(err));
-    });
+  animatronics.forEach((animatronic) => {
+    fetch(`http://${animatronic.endApi}`)
+      .then((res) => res.text())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  });
 }
 
 module.exports = {
-    startAllAnimatronics,
-    stopAllAnalimatronics
+  startAllAnimatronics,
+  stopAllAnalimatronics,
 };
