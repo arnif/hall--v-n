7477b29af238541f9500e8a5229edf595e0da0c9
Arni <arnifa@gmail.com>
2024-10-30 21:58:27 +0000

    format logger

--- logger.js
+++ logger.js
@@ -1,15 +1,14 @@
 const pino = require("pino");
 
-// Configure the logger
 const logger = pino({
-  level: process.env.LOG_LEVEL || "info", // Default to 'info' level, but can be overridden by an environment variable
+  level: process.env.LOG_LEVEL || "info",
   transport:
     process.env.NODE_ENV !== "production"
       ? {
-          target: "pino-pretty", // Use 'pino-pretty' for pretty-printing logs in non-production environments
+          target: "pino-pretty",
           options: {
-            colorize: true, // Add colors for better readability in the console
-            translateTime: "SYS:standard", // Include timestamps in logs
+            colorize: true,
+            translateTime: "HH:MM:ss", // Show only the time without date or timezone
           },
         }
       : undefined,
