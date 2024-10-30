859db58ae7e202341badd4267d0e31fea423fbbe
Arni <arnifa@gmail.com>
2024-10-30 21:44:54 +0000

    adjust cooldown

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ const volume = 30;
 const music_volume = 20;
 const PLAYLIST_NAME = "Halloween";
 
-const ADDITIONAL_COOLDOWN = 3000; // Additional cooldown time in milliseconds
+const ADDITIONAL_COOLDOWN = 30000; // Additional cooldown time in milliseconds
 
 module.exports = {
   volume,
